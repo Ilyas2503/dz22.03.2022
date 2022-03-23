45a392437d32a1ae69907314ec31d2c6159179ae
Ilyas2503 <83136147+Ilyas2503@users.noreply.github.com>
2022-03-23 13:59:47 +0600

    Initial commit

---
+++ sys.py
@@ -0,0 +1,10 @@
+"""
+import sys
+
+oc = input("Какая у вас операционная система: ")
+CPU = "2,4 GHz Intel Core i7"
+memory = "8 ГБ 1600 МГц DDR3"
+schedule = "NVIDIA GeForce GT 650M 1024 МБ"
+if oc == "2,4 GHz Intel Core i7" and memory == "8 ГБ 1600 МГц DDR3" and schedule == "NVIDIA GeForce GT 650M 1024 МБ":
+    print()
+"""
